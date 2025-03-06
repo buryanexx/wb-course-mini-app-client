26c62ed85295cd387f301efed143aff22df41018
Даниил Бурцев <theburtsev@MacBook-Air-2.local>
2025-03-07 00:59:35 +0300

    Полная очистка проекта от тестовых файлов и исправление конфигурации

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import Home from './pages/Home';
 import ModulesList from './pages/ModulesList';
 import ModuleDetail from './pages/ModuleDetail';
 import About from './pages/About';
-import TestPage from './pages/TestPage';
 import './styles/App.css';
 
 function App() {
@@ -54,7 +53,6 @@ function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/test" element={<TestPage />} />
             <Route path="/modules" element={<ModulesList />} />
             <Route path="/modules/:id" element={<ModuleDetail />} />
             <Route path="/about" element={<About />} />
