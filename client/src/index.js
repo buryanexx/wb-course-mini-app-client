1cc480cb528a5ff5bc5788d5a16571a4a062a071
Даниил Бурцев <theburtsev@MacBook-Air-2.local>
2025-03-06 19:10:53 +0300

    Принудительное обновление версии и добавление тестовой страницы v2

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import './styles/App.css';
+import './styles/Header.css';
+import './styles/Footer.css';
+import './styles/Menu.css';
+import './styles/ModuleCard.css';
+import './styles/Home.css';
 import './styles/AdaptiveContainer.css';
+import './styles/CircleNumber.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
