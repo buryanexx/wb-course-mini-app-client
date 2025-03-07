25f6ca1acaf54f986a0a2ba0eed5083b9c3f7b64
Даниил Бурцев <theburtsev@MacBook-Air-2.local>
2025-03-07 03:12:32 +0300

    Подготовка к деплою полной версии

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 // Реэкспортируем функции из api.js для обратной совместимости
-import { getModules, getModuleById, submitFeedback } from '../api/api';
+// import { getModules, getModuleById, submitFeedback } from '../api/api';
 import axios from 'axios';
 
 // Базовый URL API
